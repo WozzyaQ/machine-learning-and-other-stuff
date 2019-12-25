cd706db0ae2683eed0886d023094d1aeb34218f4
Vladyslav <46566324+WozzyaQ@users.noreply.github.com>
2019-12-25 23:49:36 +0200

    Add files via upload

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 
-def binary_search(arr, key):
+def binary_search(arr: list, key: int) -> int:
     if arr:
-        left = 0
-        right = len(arr)
+        left: int = 0
+        right: int = len(arr)
         while left < right:
-            middle = (left+right)//2
+            middle: int = (left+right)//2
             if arr[middle] > key:
                 right = middle
             else:
@@ -17,5 +17,5 @@ def binary_search(arr, key):
         return -1
 
 
-a = [item for item in range(10000)]
-print(binary_search(a,20))
+a: list = [item for item in range(10000)]
+print(binary_search(a, 20))
